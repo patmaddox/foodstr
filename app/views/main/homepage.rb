1356f3835aaf050bf4ae8be55c595d4628f7794b
Pat Maddox <pat.maddox@gmail.com>
2008-07-10 02:10:52 -0400

    basic spec for home page

--- app/views/main/homepage.rb
+++ app/views/main/homepage.rb
@@ -1,7 +1,6 @@
 class Views::Main::Homepage < Widgets::Workshopr
   def render
     h1 "Workshopr"
-
     p "Check me out!"
   end
 end
--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,10 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe MainController do
-
-  #Delete this example and add some real ones
-  it "should use MainController" do
-    controller.should be_an_instance_of(MainController)
+  describe "home page" do
+    it "should be ok" do
+      get :homepage
+      response.should be_success
+      response.should render_widget(Views::Main::Homepage)
+    end
   end
-
 end
