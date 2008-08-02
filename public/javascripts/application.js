80d3b395c39ef36f1d615a1a9dfc99caa62b306e
Pat Maddox <pat.maddox@gmail.com>
2008-08-02 17:24:37 -0400

    use jquery instead of scriptaculous/prototype

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(document).ready(function() {
+    jQuery('div.menu_item a').click(function() {
+        jQuery(this).siblings('div.feedback').show();
+    });
+});
