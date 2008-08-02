285b6b79c98f8b1169cd22a30acab8629a899433
Pat Maddox <pat.maddox@gmail.com>
2008-08-02 16:06:50 -0400

    account links and stuff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
     redirect_to(login_url) unless logged_in? && authorized?
   end
 
+  helper_method :logged_in?
   def logged_in?
     !current_user.nil?
   end
